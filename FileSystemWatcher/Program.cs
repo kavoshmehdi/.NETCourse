4ae768598a8296d91c1fc35cf4d87a9629695733
kavoshmehdi <kavoshmehdi@yahoo.com>
2020-09-14 10:38:17 +0430

    add project filesystemwatcher

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace FSW {
+    class Program {
+        static void Main (string[] args) {
+            string path = @"D:\IDG";
+            System.Console.WriteLine ("Watch Directory {0}", path);
+            MonitorDirectory (path);
+            Console.ReadKey ();
+
+            Console.WriteLine ("Hello World!");
+        }
+        private static void MonitorDirectory (string path) {
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher ();
+            fileSystemWatcher.Path = path;
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
+            fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
+            fileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+        private static void FileSystemWatcher_Created (object sender, FileSystemEventArgs e) {
+            Console.WriteLine ("File created: {0}", e.Name);
+        }
+        private static void FileSystemWatcher_Renamed (object sender, FileSystemEventArgs e) {
+            Console.WriteLine ("File renamed: {0}", e.Name);
+        }
+        private static void FileSystemWatcher_Deleted (object sender, FileSystemEventArgs e) {
+            Console.WriteLine ("File deleted: {0}", e.Name);
+        }
+        private static void FileSystemWatcher_Changed (object sender, FileSystemEventArgs e) {
+            Console.WriteLine ("File changed: {0}", e.Name);
+        }
+    }
+}
